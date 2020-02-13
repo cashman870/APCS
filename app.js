4c3c1f21feefa5af38e4d89085b172cb1842976d
Cole Ashley <coleashley@ORRHS.ORG>
2020-02-13 13:53:32 -0500

    Coding Practice

---
+++ app.js
@@ -0,0 +1,43 @@
+// const add = 7 + 3;
+// const sub = 7 - 3;
+// const mult = 7 * 3;
+// const div = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4;
+// console.log(add)
+// console.log(sub)
+// console.log(mult)
+// console.log(div)
+// console.log(paren)
+
+// let score = 0
+// console.log('Current Score: ' + score)
+// let numLikes = 7
+// console.log('So far ' + numLikes + ' people like this page')
+// let score = 0;
+// let lives = 3;
+// console.log("You have " + lives + ' lives left with score: ' + score);
+
+// let points = 1023;
+// let lives = 3;
+// let totalTime = 45;
+// console.log("Points: " + points);
+// console.log("Lives: " + lives);
+// console.log("Total Time: " + totalTime + " secs");
+// console.log("---------------------------");
+// let totalScore = (points*lives)/totalTime
+// console.log("Your Total Score is: " + totalScore);
+
+// console.log("Rolling Dice!")
+// let roll1 = Math.floor(Math.random()*(6-1))+1
+// let roll2 = Math.floor(Math.random()*(6-1))+1
+// let totalRoll = roll1 + roll2
+// console.log('You rolled a: ' + roll1)
+// console.log('and a: ' + roll2)
+// console.log('For a total of: ' + totalRoll)
+
+let a = 3
+let b = 7
+a = b
+b = a
+console.log(a)
+console.log(b)
