9ab4bf2255c22e29b1f7c05dae6b295ef5a89a87
Cole Ashley <coleashley@ORRHS.ORG>
2020-02-13 12:53:02 -0500

    Added 'view code' button

--- Clicker Game - Banana Machine/main.js
+++ Clicker Game - Banana Machine/main.js
@@ -4,7 +4,14 @@ function setup() {
   button = createButton('Start');
   button.position(400,300)
   button.mousePressed(goToLink)
+  seeCode = createButton('View the code')
+  seeCode.position(600,600)
+  seeCode.mousePressed(gitLink)
 }
 function goToLink() {
   window.location.href = 'index.html'; // Same tab
+}
+
+function gitLink(){
+  window.open('https://github.com/cashman870/APCS/tree/master/Clicker%20Game%20-%20Banana%20Machine');
 }
